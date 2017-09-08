cbd7fdeb5b06b69c4547507bbf2db541440c449b
jessededoes <dedoes@xs4all.nl>
2017-09-08 22:13:47 +0200

    .

---
+++ src/main/java/clariah/fcs/mapping/Conversions.java
@@ -0,0 +1,21 @@
+package clariah.fcs.mapping;
+
+public class Conversions 
+{
+   static  ConversionTable UD2CHN;
+   
+    static ConversionTable UD2BaB;
+    
+    
+    static
+    {
+    	
+    }
+    
+    ConversionTable UD2CGN;
+    
+    static
+    {
+        	
+    }
+}
