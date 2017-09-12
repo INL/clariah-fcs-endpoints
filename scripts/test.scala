3ad750d58563d36a093838ef6e2dddd8c2243007
jessededoes <dedoes@xs4all.nl>
2017-09-12 23:06:52 +0200

    .

--- scripts/test.scala
+++ scripts/test.scala
@@ -9,12 +9,13 @@ object FCSTest
   {
     val url = s"$server${encode(query)}&x-fcs-context=$corpus" 
     val doc = XML.load(url)
-    println(doc)
+    //println(doc)
     (doc \\ "Advanced").toList.foreach (
       a =>
       {
         val segmentIds = ((a \\ "Segment") \ "@id").toList
-        val layers = a \\ "Layer"
+        val layers = (a \\ "Layer").map(l => (l \ "@id", (l \\ "Span").map(s => (s \ "@ref" -> s.text))))
+        println(layers) 
         
         // en daarin weel de spans ...
       }
